6060d304641788fa64e4e60ea941dd84fcda7ad5
MikeKowalsky <mkowalewski83@gmail.com>
2018-11-27 22:29:13 +0100

    added route to resources from db and Schema

--- server.js
+++ server.js
@@ -1,27 +1,44 @@
 const express = require("express");
-const router = express.Router();
-const mongoose = require("mongoose");
 
 // initialize app
 const app = express();
 
 // Connect to MongoDB by Monooose
-mongoose
-  .connect("mongodb://mike:mike666@ds211504.mlab.com:11504/mern_one")
-  .then(() => console.log("MongoDB Connected"))
-  .catch(err => console.log(err));
+const mongoose = require("mongoose");
+const dev_db_url = "mongodb://mike:mike666@ds211504.mlab.com:11504/mern_one";
+const mongoDB = process.env.MONGODB_URI || dev_db_url;
+mongoose.connect(mongoDB);
+mongoose.Promise = global.Promise;
+
+const db = mongoose.connection;
+db.once("error", console.error.bind(console, "MongoDB connection error"));
+
+// old version of mongoose connect
+// mongoose
+//   .connect()
+//   .then(() => console.log("MongoDB Connected"))
+//   .catch(err => console.log(err));
 
 // app.get("/", (req, res) => res.send("Hello guy's!"));
 
-// Routes
+app.get("/cities", (req, res) => {
+  db.collection("city")
+    .find()
+    .toArray((err, results) => {
+      res.send(results);
+    });
+});
 
-// home page route
+// Routes
+// Home && test page route
+const router = express.Router();
+
 router.get("/", (req, res) => res.send("im the home page!"));
 router.get("/test", (req, res) => res.send("HELLO WORLD!"));
 
 // Use routes
 app.use("/", router);
 
-// Start the server
+// Set up the port and send a msg when start the server
 const port = process.env.PORT || 5000;
 app.listen(port, () => console.log(`Server running on port ${port}!`));
