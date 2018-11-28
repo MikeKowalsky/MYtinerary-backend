dd83c099cb5b7c69a09177071fbf98a43b6fa5b2
MikeKowalsky <mkowalewski83@gmail.com>
2018-11-28 14:34:36 +0100

    added cities/all route and tested

--- models/City.js
+++ models/City.js
@@ -1,7 +1,5 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
-//Schemas not only define the structure of your document and casting of properties, they also define document instance methods, static Model methods, compound indexes, and document lifecycle hooks called middleware.
-// https://mongoosejs.com/docs/guide.html
 
 //Create Schema
 const CitySchema = new Schema({
@@ -10,4 +8,11 @@ const CitySchema = new Schema({
 });
 
 module.exports = mongoose.model("City", CitySchema);
+
 //To use our schema definition, we need to convert our CitySchema into a Model we can work with. Doing this while exporting.
+
+//Schemas not only define the structure of your document and casting of properties, they also define document instance methods, static Model methods, compound indexes, and document lifecycle hooks called middleware.
+
+// var schema = new mongoose.Schema({ name: 'string', size: 'string' });
+// var Tank = mongoose.model('Tank', schema);
+// The first argument is the singular name of the collection your model is for. Mongoose automatically looks for the plural version of your model name. Thus, for the example above, the model Tank is for the tanks collection in the database.
--- server.js
+++ server.js
@@ -3,19 +3,7 @@ const express = require("express");
 // initialize app
 const app = express();
 
-// // Connect to MongoDB by Monooose // Schema dosn't work ??
-// const mongoose = require("mongoose");
-// const dev_db_url = "mongodb://mike:mike666@ds211504.mlab.com:11504/mern_one";
-// const mongoDB = process.env.MONGODB_URI || dev_db_url;
-// mongoose.connect(mongoDB);
-// // .then(() => console.log("MongoDB Connected")) // not sure that I need those two lines
-// // .catch(err => console.log(err)); // not sure that I need those two lines
-// mongoose.Promise = global.Promise;
-
-// const db = mongoose.connection;
-// db.once("error", console.error.bind(console, "MongoDB connection error"));
-
-// old version of mongoose connect
+// mongoose connect
 const mongoose = require("mongoose");
 const dev_db_url = "mongodb://mike:mike666@ds211504.mlab.com:11504/mern_one";
 const mongoDB = process.env.MONGODB_URI || dev_db_url;
@@ -26,18 +14,9 @@ mongoose
   .catch(err => console.log(err));
 
 const db = mongoose.connection;
-// mongoose.Promise = global.Promise;
+// mongoose.Promise = global.Promise;  // ?? don't know if I need this
 
 app.get("/", (req, res) => res.send("Hello guy's!"));
-
-// just retrieving whole collection
-app.get("/city", (req, res) => {
-  db.collection("city")
-    .find()
-    .toArray((err, results) => {
-      res.send(results);
-    });
-});
 
 // just retrieving whole collection
 // app.get("/cities", (req, res) => {
@@ -50,28 +29,9 @@ app.get("/city", (req, res) => {
 
 // retrieving using schema
 const CityModel = require("./models/City");
-// console.log(CityModel);
-// const city = new CityModel({
-//   name: "Gdansk",
-//   country: "Poland"
-// });
-
-// city
-//   .save()
-//   .then(doc => console.log(doc))
-//   .catch(err => console.log(err));
-
-// CityModel.find()
-//   .then(doc => {
-//     console.log(doc);
-//   })
-//   .catch(err => {
-//     console.error(err);
-//   });
 
 app.get("/cities/all", (req, res) => {
   CityModel.find()
-    // .then(docs => console.log(docs))
     .then(docs => res.send(docs))
     .catch(err => console.log(err));
 });
