be07b06d912c0f572dcf71e7a4a335a52b1779a9
MikeKowalsky <mkowalewski83@gmail.com>
2018-11-28 21:47:18 +0100

    added middleware to handle cors

--- server.js
+++ server.js
@@ -16,8 +16,6 @@ mongoose
 const db = mongoose.connection;
 // mongoose.Promise = global.Promise;  // ?? don't know if I need this
 
-app.get("/", (req, res) => res.send("Hello guy's!"));
-
 // just retrieving whole collection
 // app.get("/cities", (req, res) => {
 //   db.collection("cities")
@@ -27,24 +25,39 @@ app.get("/", (req, res) => res.send("Hello guy's!"));
 //     });
 // });
 
-// retrieving using schema
-const CityModel = require("./models/City");
-
-app.get("/cities/all", (req, res) => {
-  CityModel.find()
-    .then(docs => res.send(docs))
-    .catch(err => console.log(err));
+// acts as a middleware
+// to handle CORS Errors
+app.use((req, res, next) => {
+  //doesn't send response - just adjust it
+  res.header("Access-Control-Allow-Origin", "*"); //* to give access to any origin
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requestes-With, Content-Type, Accept, Authorization" //to give access to all the headers provided
+  );
+  if (req.method === "OPTIONS") {
+    res.header("Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET"); //to give access to all the methoda provided
+    return res.status(200).json({});
+  }
+  next(); //so that other routes can take over
 });
 
 // Routes
 // Home && test page route
 const router = express.Router();
 
-router.get("/", (req, res) => res.send("im the home page!"));
-router.get("/test", (req, res) => res.send("HELLO WORLD!"));
-
 // Use routes
 app.use("/", router);
+
+router.get("/", (req, res) => res.send("Hello guy's! This is home page!"));
+router.get("/test", (req, res) => res.send("HELLO WORLD!"));
+
+// retrieving using schema
+const CityModel = require("./models/City");
+router.get("/cities/all", (req, res) => {
+  CityModel.find()
+    .then(docs => res.send(docs))
+    .catch(err => console.log(err));
+});
 
 // Set up the port and send a msg when start the server
 const port = process.env.PORT || 5000;
