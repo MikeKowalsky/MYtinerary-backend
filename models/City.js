3c0f33937ce93b39fa2064197b8f17abb2ed9839
MikeKowalsky <mkowalewski83@gmail.com>
2018-12-11 18:18:41 +0100

    working on itineraries collections

--- models/City.js
+++ models/City.js
@@ -1,5 +1,6 @@
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+// const Itinerary = require("./Itinerary");
 
 //Create Schema
 const schema = new Schema({
--- server.js
+++ server.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const bodyParser = require("body-parser");
 
 // initialize app
 const app = express();
+
+// body parser middelware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 // mongoose connect
 const mongoose = require("mongoose");
@@ -9,7 +14,10 @@ const dev_db_url = "mongodb://mike:mike666@ds211504.mlab.com:11504/mern_one";
 const mongoDB = process.env.MONGODB_URI || dev_db_url;
 
 mongoose
-  .connect(mongoDB)
+  .connect(
+    mongoDB,
+    { useNewUrlParser: true }
+  )
   .then(() => console.log("MongoDB Connected"))
   .catch(err => console.log(err));
 
@@ -57,6 +65,81 @@ router.get("/cities/all", (req, res) => {
   CityModel.find()
     .then(docs => res.send(docs))
     .catch(err => console.log(err));
+});
+
+const ItineraryModel = require("./models/Itinerary");
+router.get("/itineraries", (req, res) => {
+  ItineraryModel.find()
+    .then(docs => res.send(docs))
+    .catch(err => console.log(err));
+});
+
+router.post("/addData", (req, res) => {
+  console.log(req.body);
+
+  // const newCity = new CityModel({
+  //   _id: new mongoose.Types.ObjectId,
+  //   name: "Barcelona",
+  //   country: "Spain"
+  // })
+
+  CityModel.findOne({ name: "Barcelona" }, (err, city) => {
+    if (err) console.log(err);
+
+    console.log(city._id);
+
+    const newItinerary = new ItineraryModel({
+      _id: new mongoose.Types.ObjectId(),
+      author: req.body.author,
+      city: city.id,
+      name: req.body.name,
+      rating: req.body.rating,
+      likes: req.body.likes,
+      duration: req.body.duration,
+      priceRange: req.body.priceRange,
+      tags: req.body.tags,
+      activities: req.body.activities
+    });
+
+    newItinerary.save(err => {
+      if (err) console.log(err);
+    });
+
+    // newItinerary.populate("city").exec(function(err, itinerary) {
+    //   if (err) console.log(err);
+
+    //   console.log("The city is %s", itinerary.city.name);
+    // });
+
+    // Story.
+    // findOne({ title: 'Casino Royale' }).
+    // populate('author').
+    // exec(function (err, story) {
+    //   if (err) return handleError(err);
+    //   console.log('The author is %s', story.author.name);
+    //   // prints "The author is Ian Fleming"
+    // });
+
+    res.send("created new Itinerary");
+  });
+
+  // res.send("post ok");
+  // "author": "GaudiLover",
+  //   "name": "Gaudi In A Day",
+  //   "rating": 4.67,
+  //   "likes": 34,
+  //   "duration": 12,
+  //   "priceRange": 2,
+  //   "tags": [
+  //       "Art",
+  //       "Architecture",
+  //       "History"
+  //   ],
+  //   "activities": [
+  //       "Casa Balto",
+  //       "Le Pedrera",
+  //       "Sagrada Familia"
+  //   ]
 });
 
 // Set up the port and send a msg when start the server
