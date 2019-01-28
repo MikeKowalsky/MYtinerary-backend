e8c3cca549b2de82f0aaa7515319cfd9806ce9b6
MikeKowalsky <mkowalewski83@gmail.com>
2019-01-28 16:00:57 +0100

    implemented bcrypt and db.save

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const router = express.Router();
+const bcrypt = require("bcryptjs");
 
 // Load users model
 const User = require("../../models/User");
@@ -14,19 +15,42 @@ router.get("/test", (req, res) => res.json({ msg: "Testing users routes" }));
 // @access  Public
 router.post("/register", (req, res) => {
   User.findOne({ email: req.body.email }).then(user => {
-    if (user) {
-      res.json({ error: "User already exist! Email needs to be unique." });
-    } else {
-      const avatar = req.body.avatar || null;
+    if (user)
+      res
+        .status(400)
+        .json({ error: "User already exist! Email needs to be unique." });
 
-      const newUser = {
-        name: req.body.name,
-        email: req.body.email,
-        avatar,
-        password: req.body.password
-      };
-      res.json(newUser);
-    }
+    const { name, email, password } = req.body;
+    const avatar = req.body.avatar || null;
+
+    const newUser = new User({
+      name,
+      email,
+      avatar,
+      password
+    });
+
+    bcrypt.genSalt(10, (err, salt) => {
+      bcrypt.hash(newUser.password, salt, (err, hash) => {
+        if (err) throw err;
+        newUser.password = hash;
+        newUser
+          .save()
+          .then(user => res.status(201).json(user))
+          .catch(err => console.log(err));
+      });
+    });
+  });
+});
+
+// @route   POST api/users/register
+// @desc    Register new user
+// @access  Public
+router.post("/login", (req, res) => {
+  const { email, password } = req.body;
+
+  User.findOne({ email }).then(user => {
+    if (!user) res.status(404).json({ error: "User not found" });
   });
 });
 
